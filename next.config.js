b9a238d4e90dc408457b59c6b20a290ec2e98553
iMac <imac@iMac.local>
2025-04-18 16:15:06 +0100

    docs: Add KGoT documentation for Vercel hosting

---
+++ next.config.js
@@ -0,0 +1,20 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.mdx?$/,
+      use: [
+        {
+          loader: '@mdx-js/loader',
+          options: {}
+        }
+      ]
+    });
+
+    return config;
+  }
+};
+
+export default nextConfig; 
